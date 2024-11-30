105293bcfa58415405cd0253705ad0a1242c4d9c
cp-20 <n.naoki.1587@gmail.com>
2024-12-01 00:06:25 +0900

    🍿 🧈 🍄 postgresql init.sh

--- postgresql/base/config/init.sh
+++ postgresql/base/config/init.sh
@@ -6,3 +6,5 @@ CREATE USER IF NOT EXISTS traqing WITH PASSWORD '${POSTGRES_PASSWORD_TRAQING}';
 CREATE DATABASE IF NOT EXISTS traqing;
 GRANT ALL PRIVILEGES ON DATABASE traqing TO traqing;
 EOF
+
+psql -U postgres -f /docker-entrypoint-initdb.d/init.sql
