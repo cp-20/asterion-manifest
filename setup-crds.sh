44dfa506ad612478ad25138e9e9e81408fb2eed7
cp-20 <n.naoki.1587@gmail.com>
2025-06-06 00:55:01 +0900

    setup-crds

---
+++ setup-crds.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p .crd
+cd .crd || exit
+
+# renovate:github-url
+wget https://raw.githubusercontent.com/yannh/kubeconform/v0.7.0/scripts/openapi2jsonschema.py
+export FILENAME_FORMAT='{fullgroup}-{kind}-{version}'
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://raw.githubusercontent.com/argoproj/argo-cd/v3.0.5/manifests/install.yaml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://raw.githubusercontent.com/traefik/traefik/v3.4.1/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+# renovate:github-url
+python3 openapi2jsonschema.py https://github.com/cert-manager/cert-manager/releases/download/v1.17.2/cert-manager.yaml
